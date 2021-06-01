bbaeaf0d743c2996e99615e450789553defbca4a
aidan <aidansuss@gmail.com>
2021-06-02 08:41:31 +1000

    Uploading challange 5, static void

--- mean_median.py
+++ mean_median.py
@@ -1,12 +1,33 @@
 # M0_C5 - Mean, Median
+import math
 
 def mean(scores):
-    # Write your code here
-    return "not implemented"
+    scores_valid = []
+    for score in scores:    
+        if score >= 0 and score <= 100:
+            scores_valid.append(score)
+    tests = len(scores_valid)
+    test_scores = sum(scores_valid)
+    if len(scores_valid) == 0:
+        mean = -1
+    else:
+        mean = math.floor(test_scores/tests)
+    return mean
 
 def median(scores):
-    # Write your code here
-    return "not implemented"
+    scores_valid = []
+    for score in scores:    
+        if score >= 0 and score <= 100:
+            scores_valid.append(score)
+    tests = len(scores_valid)
+    if len(scores_valid) == 0:
+        median = -1
+    else:
+        if tests % 2 == 0:
+            median = math.floor( (scores_valid[int(tests/2-1)] + scores_valid[int(tests/2)])/2 )
+        else:
+            median = math.floor( scores_valid[math.ceil(tests/2)-1] )
+    return median
 
 if __name__ == '__main__':
     scores = input("Input list of test scores, space-separated: ")
