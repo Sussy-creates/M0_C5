4569047df36d6256bc707112c128806919e74121
aidan <aidansuss@gmail.com>
2021-06-02 09:01:54 +1000

    Cleaning up code for challenge 5

--- mean_median.py
+++ mean_median.py
@@ -1,5 +1,4 @@
 # M0_C5 - Mean, Median
-import math
 
 def mean(scores):
     scores_valid = []
@@ -7,12 +6,11 @@ def mean(scores):
         if score >= 0 and score <= 100:
             scores_valid.append(score)
     tests = len(scores_valid)
-    test_scores = sum(scores_valid)
+    total = sum(scores_valid)
     if len(scores_valid) == 0:
-        mean = -1
+        return -1
     else:
-        mean = math.floor(test_scores/tests)
-    return mean
+        return int(total/tests)
 
 def median(scores):
     scores_valid = []
@@ -21,13 +19,12 @@ def median(scores):
             scores_valid.append(score)
     tests = len(scores_valid)
     if len(scores_valid) == 0:
-        median = -1
+        return -1
     else:
         if tests % 2 == 0:
-            median = math.floor( (scores_valid[int(tests/2-1)] + scores_valid[int(tests/2)])/2 )
+            return int((scores_valid[tests//2-1] + scores_valid[tests//2])/2)
         else:
-            median = math.floor( scores_valid[math.ceil(tests/2)-1] )
-    return median
+            return int(scores_valid[tests//2])
 
 if __name__ == '__main__':
     scores = input("Input list of test scores, space-separated: ")
